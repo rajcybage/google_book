c404ab06c4af3333083d4a35173ab2552687f83c
Rajarshi Das <rajarshid@cybage.com>
2013-08-01 16:13:36 +0530

    fis duplication

--- lib/google_book/book_info.rb
+++ lib/google_book/book_info.rb
@@ -2,28 +2,22 @@ require_relative 'base.rb'
 require_relative 'book_item.rb'
 class BookInfo 
   attr_accessor :items, :titles, :subtitles, :book
-  
+  @titles = []
+  @subtitles = []
   def initialize(items=[])
     @items = items
   end	
 
   #return the all titles of those books 
   def get_all_titles
-   	@titles = []
-    @items[:items].each do |item|
-      @titles << item["volumeInfo"]["title"]
-      
-    end
-    return @titles	
+    show_all_titles_and_subtitles("title")
+    return @titles
   end
   
   #return all the subtitles
   def get_all_sub_titles
-    @subtitles = []
-    @items[:items].each do |item|
-      @subtitles << item["volumeInfo"]["subtitle"]
-    end
-    return @subtitles	
+    show_all_titles_and_subtitles("subtitle")
+    return @subtitles
   end 
 
 
@@ -32,4 +26,12 @@ class BookInfo
     item = @items.detect{|i| i[params.keys.first] == params.values.first} || @items.detect{|i| i["volumeInfo"][params.keys.first] == params.values.first}
     @book = BookItem.new(item: item)
   end
+
+  private
+  def show_all_titles_and_subtitles(flag)
+    @items[:items].each do |item|
+      @titles << item["volumeInfo"]["title"]
+    end
+  end
+
 end
